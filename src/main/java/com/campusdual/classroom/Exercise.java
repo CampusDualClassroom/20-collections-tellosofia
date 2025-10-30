37696aaa5c609a0fac175ae9103136f5fbeae218
Sofía Tello <tellosofia@outlook.com>
2025-10-30 17:37:22 +0100

    Exercise 20: Solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,22 +1,34 @@
 package com.campusdual.classroom;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise {
 
     public static List<Person> getPeopleList() {
+        Person person = new Person("John", "Smith");
+        Teacher teacher = new Teacher("María", "Montessori", "Educación");
+        PoliceOfficer police = new PoliceOfficer("Jake", "Peralta", "B-99");
+        Doctor doctor = new Doctor("Gregory", "House", "Nefrología e Infectología");
 
+        List<Person> personList = new ArrayList<>();
+
+        personList.add(person);
+        personList.add(teacher);
+        personList.add(police);
+        personList.add(doctor);
+
+        return personList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person personString : stringList) {
+            personString.getDetails();
+        }
     }
 
     public static void main(String[] args) {
         List<Person> stringList = getPeopleList();
         showPeopleDetails(stringList);
     }
-
-
-
 }
